9b3d4b9c6ef5f49e142dae0a486573ab5d591203
cloudstacker <iamjayakrishna7@gmail.com>
2025-10-25 16:19:14 +0530

    commit with linear search examples

---
+++ 03_Linear_search/src/com/linear/MaxWealth.java
@@ -0,0 +1,30 @@
+package com.linear;
+
+public class MaxWealth {
+    public static void main(String[] args) {
+
+        int[][] accounts = {{1,2,3},{3,2,1}};
+        System.out.println(maximumWealth(accounts));
+
+
+
+    }
+    static int maximumWealth(int[][] accounts) {
+        // person = rol
+        // account = col
+        int ans = Integer.MIN_VALUE;
+        for (int[] persons : accounts) {
+            // when you start a new row, take a new sum for that row
+            int sum = 0;
+            for (int acnt : persons) {
+                sum += acnt;
+            }
+            // now we have sum of accounts of person
+            // check with overall ans
+            if (sum > ans) {
+                ans = sum;
+            }
+        }
+        return ans;
+    }
+}
