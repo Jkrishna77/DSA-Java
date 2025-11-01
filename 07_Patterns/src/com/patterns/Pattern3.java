828129f624d07a053d3887c59f7ea85fb221b6c0
cloudstacker <iamjayakrishna7@gmail.com>
2025-11-01 10:05:43 +0530

    pattern problem 3

---
+++ 07_Patterns/src/com/patterns/Pattern3.java
@@ -0,0 +1,19 @@
+package com.patterns;
+/*
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        for(int row=5;row>0;row--){
+            for(int col=row;col>0;col--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
