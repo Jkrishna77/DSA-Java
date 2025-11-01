90d977e0a1f0cf7e12ae59d4cada1cc355bcf68b
cloudstacker <iamjayakrishna7@gmail.com>
2025-11-01 10:03:15 +0530

    pattern problem 2

---
+++ 07_Patterns/src/com/patterns/Pattern2.java
@@ -0,0 +1,20 @@
+package com.patterns;
+
+/*
+*
+* *
+* * *
+* * * *
+* * * * *
+ */
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        for(int row=1;row<6;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
